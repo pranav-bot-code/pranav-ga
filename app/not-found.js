3fb0fcaf0f2775063532a40b9de16886783ffae3
pranav-bot-code <pkk02008@gmail.com>
2023-05-09 15:38:37 +0530

    initial commit

---
+++ app/not-found.js
@@ -0,0 +1,7 @@
+export default function NotFound(){
+     return (
+          <div className="bg-gradient-to-br from-emerald-500 to-lime-300 via-green-400 flex items-center justify-center bg-white h-screen w-screen">
+               <h1 className="text-3xl font-semibold font-mono">404 | The page you were looking for was not found</h1>
+          </div>
+     )
+}
--- app/page.js
+++ app/page.js
@@ -1,113 +1,82 @@
-import Image from 'next/image'
+import { TbSunset2 } from "react-icons/tb";
+import { RiScissorsCutFill } from "react-icons/ri";
+import { FiDownload } from "react-icons/fi";
+import { BsMusicNoteBeamed, BsArrowRightShort } from "react-icons/bs";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <div className="bg-white text-gray-900 w-full overflow-x-hidden h-full">
+      <div className="sticky backdrop-blur-3xl flex flex-col md:flex-row items-center justify-between">
+        <h1 className="font-bold font-mono text-4xl p-3">pranav.ga</h1>
+        <div className="flex items-center p-3 font-sans font-semibold">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="https://weather.pranav.ga"
+            className="px-2 hover:text-blue-700"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            Weather
+          </a>
+          <a
+            href="https://shorty.pranav.ga"
+            className="px-2 hover:text-blue-700"
+          >
+            URL Shortener
+          </a>
+          <a href="https://yt.pranav.ga" className="px-2 hover:text-blue-700">
+            YT Downloader
+          </a>
+          <a
+            href="https://notify.pranav.ga"
+            className="px-2 hover:text-blue-700"
+          >
+            Notify
           </a>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex items-center justify-center md:justify-normal h-96 bg-cover bg-[url('/background-1.jpeg')]">
+        <div className="p-10 m-10 items-center flex-col backdrop-blur-3xl backdrop-brightness-150 rounded-lg flex">
+          <h1 className="text-2xl font-mono font-bold">pranav.ga</h1>
+          <p>Easy to use and convenient <br></br>online tools!</p>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-2xl font-sans font-semibold p-3 border-b border-gray-300">
+          Tools
+        </h1>
+        <div className="flex flex-col md:flex-row p-3">
+          <div className="flex flex-col justify-start rounded-xl backdrop-brightness-90 card p-2 m-2">
+            <div className="flex items-center p-2">
+              <TbSunset2 className="text-4xl mr-2" />
+              <h1 className="font-medium">Weather</h1>
+            </div>
+            <p className="text-sm p-2">Simple Weather app</p>
+            <a className="flex justify-center items-center bg-yellow-400 hover:bg-yellow-300 p-2 rounded-xl hover:text-gray-600 cursor-pointer w-1/2 text-gray-700" href="https://weather.pranav.ga">Try it <BsArrowRightShort /></a>
+          </div>
+          <div className="flex flex-col justify-start rounded-xl backdrop-brightness-90 card p-2 m-2">
+            <div className="flex items-center p-2">
+              <RiScissorsCutFill className="text-4xl mr-2" />
+              <h1 className="font-medium">URL Shortener</h1>
+            </div>
+            <p className="text-sm p-2">A Temp URL Shortener</p>
+            <a className="flex justify-center items-center bg-blue-400 hover:bg-blue-300 p-2 rounded-xl hover:text-gray-600 cursor-pointer w-1/2 text-gray-700" href="https://shorty.pranav.ga">Try it <BsArrowRightShort /></a>
+          </div>
+          <div className="flex flex-col justify-start rounded-xl backdrop-brightness-90 card p-2 m-2">
+            <div className="flex items-center p-2">
+              <FiDownload className="text-4xl mr-2" />
+              <h1 className="font-medium">YT Downloader</h1>
+            </div>
+            <p className="text-sm p-2">A Youtube Video Downloader</p>
+            <a className="flex justify-center items-center bg-red-500 hover:bg-red-400 p-2 rounded-xl hover:text-gray-100 cursor-pointer w-2/5 text-gray-200" href="https://yt.pranav.ga">Try it <BsArrowRightShort /></a>
+          </div>
+          <div className="flex flex-col justify-start rounded-xl backdrop-brightness-90 card p-2 m-2">
+            <div className="flex items-center p-2">
+              <BsMusicNoteBeamed className="text-3xl mr-2" />
+              <h1 className="font-medium">Notify</h1>
+            </div>
+            <p className="text-sm p-2">A Simple Audio Player</p>
+            <a className="flex justify-center items-center bg-green-400 hover:bg-green-300 p-2 rounded-xl hover:text-gray-600 cursor-pointer w-1/2 text-gray-700" href="https://notify.pranav.ga">Try it <BsArrowRightShort /></a>
+          </div>
+        </div>
       </div>
-    </main>
-  )
+    </div>
+  );
 }
